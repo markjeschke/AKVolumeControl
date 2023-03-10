0c01a181d0521a1366aeab545b1b481d732e14e5
Mark Jeschke <markjeschke@yahoo.com>
2023-03-10 15:51:51 -0800

    Initial Commit

---
+++ AKVolumeControl/AKVolumeControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  AKVolumeControlApp.swift
+//  AKVolumeControl
+//
+//  Created by Mark Jeschke on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AKVolumeControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
